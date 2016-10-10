a6d8f1e0d1169ad0b6a59375c6c3dc48d2ac7585
neiderm <ded.reckning@gmail.com>
2016-10-10 13:37:29 -0500

    now with window boundary checking!

--- sdlstuff/lazy.cpp
+++ sdlstuff/lazy.cpp
@@ -16,42 +16,42 @@ const int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
 //Texture wrapper class
 class LTexture
 {
-	public:
-		//Initializes variables
-		LTexture();
+public:
+    //Initializes variables
+    LTexture();
 
-		//Deallocates memory
-		~LTexture();
+    //Deallocates memory
+    ~LTexture();
 
-		//Loads image at specified path
-		bool loadFromFile( std::string path );
+    //Loads image at specified path
+    bool loadFromFile( std::string path );
 
-		//Deallocates texture
-		void free();
+    //Deallocates texture
+    void free();
 
-		//Set color modulation
-		void setColor( Uint8 red, Uint8 green, Uint8 blue );
+    //Set color modulation
+    void setColor( Uint8 red, Uint8 green, Uint8 blue );
 
-		//Set blending
-		void setBlendMode( SDL_BlendMode blending );
+    //Set blending
+    void setBlendMode( SDL_BlendMode blending );
 
-		//Set alpha modulation
-		void setAlpha( Uint8 alpha );
+    //Set alpha modulation
+    void setAlpha( Uint8 alpha );
 
-		//Renders texture at given point
-		void render( int x, int y, SDL_Rect* clip = NULL );
+    //Renders texture at given point
+    void render( int x, int y, SDL_Rect* clip = NULL );
 
-		//Gets image dimensions
-		int getWidth();
-		int getHeight();
+    //Gets image dimensions
+    int getWidth();
+    int getHeight();
 
-	private:
-		//The actual hardware texture
-		SDL_Texture* mTexture;
+private:
+    //The actual hardware texture
+    SDL_Texture* mTexture;
 
-		//Image dimensions
-		int mWidth;
-		int mHeight;
+    //Image dimensions
+    int mWidth;
+    int mHeight;
 };
 
 //Starts up SDL and creates window
@@ -73,174 +73,174 @@ SDL_Renderer* gRenderer = NULL;
 
 LTexture::LTexture()
 {
-	//Initialize
-	mTexture = NULL;
-	mWidth = 0;
-	mHeight = 0;
+    //Initialize
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
 }
 
 LTexture::~LTexture()
 {
-	//Deallocate
-	free();
+    //Deallocate
+    free();
 }
 
 bool LTexture::loadFromFile( std::string path )
 {
-	//Get rid of preexisting texture
-	free();
+    //Get rid of preexisting texture
+    free();
 
-	//The final texture
-	SDL_Texture* newTexture = NULL;
+    //The final texture
+    SDL_Texture* newTexture = NULL;
 
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-	}
-	else
-	{
-		//Color key image
-		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else
+    {
+        //Color key image
+        SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
 
-		//Create texture from surface pixels
+        //Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-		if( newTexture == NULL )
-		{
-			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-		}
-		else
-		{
-			//Get image dimensions
-			mWidth = loadedSurface->w;
-			mHeight = loadedSurface->h;
-		}
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+        else
+        {
+            //Get image dimensions
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
+        }
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface( loadedSurface );
-	}
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
 
-	//Return success
-	mTexture = newTexture;
-	return mTexture != NULL;
+    //Return success
+    mTexture = newTexture;
+    return mTexture != NULL;
 }
 
 void LTexture::free()
 {
-	//Free texture if it exists
-	if( mTexture != NULL )
-	{
-		SDL_DestroyTexture( mTexture );
-		mTexture = NULL;
-		mWidth = 0;
-		mHeight = 0;
-	}
+    //Free texture if it exists
+    if( mTexture != NULL )
+    {
+        SDL_DestroyTexture( mTexture );
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
 }
 
 void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
 {
-	//Modulate texture rgb
-	SDL_SetTextureColorMod( mTexture, red, green, blue );
+    //Modulate texture rgb
+    SDL_SetTextureColorMod( mTexture, red, green, blue );
 }
 
 void LTexture::setBlendMode( SDL_BlendMode blending )
 {
-	//Set blending function
-	SDL_SetTextureBlendMode( mTexture, blending );
+    //Set blending function
+    SDL_SetTextureBlendMode( mTexture, blending );
 }
 
 void LTexture::setAlpha( Uint8 alpha )
 {
-	//Modulate texture alpha
-	SDL_SetTextureAlphaMod( mTexture, alpha );
+    //Modulate texture alpha
+    SDL_SetTextureAlphaMod( mTexture, alpha );
 }
 
 void LTexture::render( int x, int y, SDL_Rect* clip )
 {
-	//Set rendering space and render to screen
-	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+    //Set rendering space and render to screen
+    SDL_Rect renderQuad = { x, y, mWidth, mHeight };
 
-	//Set clip rendering dimensions
-	if( clip != NULL )
-	{
-		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
-	}
+    //Set clip rendering dimensions
+    if( clip != NULL )
+    {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
 
-	//Render to screen
-	SDL_RenderCopy( gRenderer, mTexture, clip, &renderQuad );
+    //Render to screen
+    SDL_RenderCopy( gRenderer, mTexture, clip, &renderQuad );
 }
 
 int LTexture::getWidth()
 {
-	return mWidth;
+    return mWidth;
 }
 
 int LTexture::getHeight()
 {
-	return mHeight;
+    return mHeight;
 }
 
 bool init()
 {
-	//Initialization flag
-	bool success = true;
+    //Initialization flag
+    bool success = true;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Set texture filtering to linear
+        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+        {
+            printf( "Warning: Linear texture filtering not enabled!" );
+        }
 
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Create vsynced renderer for window
+            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
 
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+            if( gRenderer == NULL )
+            {
+                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Initialize renderer color
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                {
+                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                    success = false;
+                }
+            }
+        }
+    }
 
-	return success;
+    return success;
 }
 
 bool loadMedia()
 {
-	//Loading success flag
-	bool success = true;
+    //Loading success flag
+    bool success = true;
 
     return success; // GN: nothing to see here
 
@@ -249,162 +249,178 @@ bool loadMedia()
 void close()
 {
 
-	//Destroy window
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
+    //Destroy window
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+    gRenderer = NULL;
 
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
 }
 
 
 typedef struct
 {
-		//The X and Y offsets of the dot
-		int mPosX, mPosY;
-		int mDeltaX, mDeltaY;
+    //The X and Y offsets of the dot
+    int mPosX, mPosY;
+    int mDeltaX, mDeltaY;
 } dot_t;
 
 
 int lazy_main( int argc, char* args[] )
 {
     dot_t dot;
-    dot.mPosX = 0;
-    dot.mPosY = 0;
+    dot.mPosX = 1;
+    dot.mPosY = 1;
     dot.mDeltaX = 0;
     dot.mDeltaY = 0;
 
-	//Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Load media
-		if( !loadMedia() )
-		{
-			printf( "Failed to load media!\n" );
-		}
-		else
-		{
-			//Main loop flag
-			bool quit = false;
+    //Start up SDL and create window
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
+    }
+    else
+    {
+        //Load media
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {
+            //Main loop flag
+            bool quit = false;
 
-			//Event handler
-			SDL_Event e;
+            //Event handler
+            SDL_Event e;
 
-			//Current animation frame
-			int frame = 0;
+            //Current animation frame
+            int frame = 0;
 
-			//The clock time when the timer started
-			int mStartTicks;
-
-
-			//Clear screen
-			SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-			SDL_RenderClear( gRenderer );
+            //The clock time when the timer started
+            int mStartTicks;
 
 
-			//While application is running
-			while( !quit )
-			{
-				//Start cap timer
-			    //Get the current clock time
-				mStartTicks = SDL_GetTicks(); // capTimer.start();
+            //Clear screen
+            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+            SDL_RenderClear( gRenderer );
 
 
-				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
-					//User presses a key
-					else if( e.type == SDL_KEYDOWN )
-					{
-						//Select surfaces based on key press
-						switch( e.key.keysym.sym )
-						{
-							case SDLK_UP:
-							dot.mDeltaY = -1;
-							break;
+            //While application is running
+            while( !quit )
+            {
+                //Start cap timer
+                //Get the current clock time
+                mStartTicks = SDL_GetTicks(); // capTimer.start();
 
-							case SDLK_DOWN:
-							dot.mDeltaY = +1;
-							break;
 
-							case SDLK_LEFT:
-							dot.mDeltaX = -1;
-							break;
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                    //User presses a key
+                    else if( e.type == SDL_KEYDOWN )
+                    {
+                        //Select surfaces based on key press
+                        switch( e.key.keysym.sym )
+                        {
+                        case SDLK_UP:
+                            //if (dot.mPosY > 0)
+                            dot.mDeltaY = -1;
+                            break;
 
-							case SDLK_RIGHT:
-							dot.mDeltaX = +1;
-							break;
+                        case SDLK_DOWN:
+                            //if (dot.mPosY < SCREEN_HEIGHT)
+                            dot.mDeltaY = +1;
+                            break;
 
-							default:
-							break;
-						}
-					}
-					else if( e.type == SDL_KEYUP )
-					{
-						//Select surfaces based on key press
-						switch( e.key.keysym.sym )
-						{
-							case SDLK_UP:
-							if (dot.mDeltaY == -1)  dot.mDeltaY = 0;
-							break;
+                        case SDLK_LEFT:
+                            //if (dot.mPosX > 0)
+                            dot.mDeltaX = -1;
+                            break;
 
-							case SDLK_DOWN:
-							if (dot.mDeltaY == +1)  dot.mDeltaY = 0;
-							break;
+                        case SDLK_RIGHT:
+                            //if (dot.mPosY < SCREEN_WIDTH)
+                            dot.mDeltaX = +1;
+                            break;
 
-							case SDLK_LEFT:
-							if (dot.mDeltaX == -1)  dot.mDeltaX = 0;
-							break;
+                        case SDLK_PLUS:
+                            break;
 
-							case SDLK_RIGHT:
-							if (dot.mDeltaX == +1)  dot.mDeltaX = 0;
-							break;
+                        case SDLK_MINUS:
+                            break;
 
-							default:
-							break;
-						}
-					}
-				}
+                        default:
+                            break;
+                        }
+                    }
+                    else if( e.type == SDL_KEYUP )
+                    {
+                        //Select surfaces based on key press
+                        switch( e.key.keysym.sym )
+                        {
+                        case SDLK_UP:
+                            if (dot.mDeltaY == -1)  dot.mDeltaY = 0;
+                            break;
 
-                dot.mPosX += dot.mDeltaX;
-                dot.mPosY += dot.mDeltaY;
+                        case SDLK_DOWN:
+                            if (dot.mDeltaY == +1)  dot.mDeltaY = 0;
+                            break;
 
-				//Draw dot
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );
+                        case SDLK_LEFT:
+                            if (dot.mDeltaX == -1)  dot.mDeltaX = 0;
+                            break;
+
+                        case SDLK_RIGHT:
+                            if (dot.mDeltaX == +1)  dot.mDeltaX = 0;
+                            break;
+
+                        default:
+                            break;
+                        }
+                    }
+                }
+                int tmp;
+
+                tmp = dot.mPosX + dot.mDeltaX;
+                if (tmp >= 0 && tmp < SCREEN_WIDTH)
+                    dot.mPosX = tmp;
+
+                tmp = dot.mPosY + dot.mDeltaY;
+                if (tmp >= 0 && tmp < SCREEN_HEIGHT)
+                    dot.mPosY = tmp;
+
+                //Draw dot
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );
                 SDL_RenderDrawPoint( gRenderer, dot.mPosX , dot.mPosY  );
 
 
-				//Update screen
-				SDL_RenderPresent( gRenderer );
+                //Update screen
+                SDL_RenderPresent( gRenderer );
 
 
-				//If frame finished early
-				int frameTicks = SDL_GetTicks() - mStartTicks; // capTimer.getTicks();
-				if( frameTicks < SCREEN_TICK_PER_FRAME )
-				{
-					//Wait remaining time
-					SDL_Delay( SCREEN_TICK_PER_FRAME - frameTicks );
-				}
+                //If frame finished early
+                int frameTicks = SDL_GetTicks() - mStartTicks; // capTimer.getTicks();
+                if( frameTicks < SCREEN_TICK_PER_FRAME )
+                {
+                    //Wait remaining time
+                    SDL_Delay( SCREEN_TICK_PER_FRAME - frameTicks );
+                }
 
-			}
+            }
 
-		}
-	}
+        }
+    }
 
-	//Free resources and close SDL
-	close();
+    //Free resources and close SDL
+    close();
 
-	return 0;
+    return 0;
 }
